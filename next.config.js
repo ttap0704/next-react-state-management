224cc682e6ed7eb0d9a4d0140d60ea5bc4270eda
LeeDaeGyu <ttap0704@naver.com>
2023-06-30 11:47:03 +0900

    NPM -> yarn berry 마이그레이션

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["mysql2"],
+    fontLoaders: [{loader: "@next/font/google", options: {subsets: ["latin"]}}],
   },
 };
 
